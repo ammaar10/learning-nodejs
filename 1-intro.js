6b2658f6cbbc90523d958d15bbf88d3981b3a69f
ammaar10 <nammaar1@gmail.com>
2026-07-27 23:58:28 +0530

    upto module tutorial

---
+++ 1-intro.js
@@ -0,0 +1,10 @@
+
+const number = 100
+
+if (number < 12){
+    console.log("small number");
+} else {
+    console.log("large number");
+}
+
+console.log(`Hello world`);
---
+++ app.js
@@ -0,0 +1,11 @@
+// Modules
+
+const john = "John"
+const peter = "Peter"
+const sayHi = (name) => {
+    console.log(`Hi, my name is ${name}.`)
+}
+
+sayHi("Ammmar")
+sayHi(john)
+sayHi(peter)
