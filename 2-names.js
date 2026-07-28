205d6d4dfe2d1d3550d3fcc2e091cb6b057bad18
ammaar10 <nammaar1@gmail.com>
2026-07-28 17:15:29 +0530

    first module

---
+++ 2-names.js
@@ -0,0 +1,8 @@
+//share
+const john = "John"
+const peter = "Peter"
+
+//local
+const secret = "secret"
+
+module.exports = {john,peter}
--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 // Modules
+const names = require("./2-names");
+const sayHi = require("./3-utils");
 
-const john = "John"
-const peter = "Peter"
-const sayHi = (name) => {
-    console.log(`Hi, my name is ${name}.`)
-}
+
 
 sayHi("Ammmar")
-sayHi(john)
-sayHi(peter)
+sayHi(names.john)
+sayHi(names.peter)
