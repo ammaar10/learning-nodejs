83fb1b91070248e82c71dff576e32c4b1a57c524
ammaar10 <nammaar1@gmail.com>
2026-07-29 19:57:19 +0530

    fs-async.js

---
+++ 5-modules.js
@@ -0,0 +1,9 @@
+// Modules
+const names = require("./2-names");
+const sayHi = require("./3-utils");
+require("./4-mind-grenade~")
+
+
+sayHi("Ammmar")
+sayHi(names.john)
+sayHi(names.peter)
--- app.js
+++
@@ -1,9 +0,0 @@
-// Modules
-const names = require("./2-names");
-const sayHi = require("./3-utils");
-
-
-
-sayHi("Ammmar")
-sayHi(names.john)
-sayHi(names.peter)
